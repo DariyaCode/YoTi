c40edee6ca4e5dec987ef34b94f897365e5c136e
Dariya Gecher <dariya.gecher@gmail.com>
2023-03-30 15:54:30 +0600

    Try to doing Read in CRUD(for example on loadData function)

--- YoTi/Controllers/TodoListViewController.swift
+++ YoTi/Controllers/TodoListViewController.swift
@@ -10,16 +10,15 @@ import CoreData
 
 class TodoListViewController: UITableViewController {
     var itemArray = [Item]()
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(dataFilePath)
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
-//        loadItems()
+        loadItems()
     }
 
     //MARK: - Tableview Datasource Methods
@@ -92,16 +91,14 @@ class TodoListViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-//    func loadItems() {
-//        if let data = try? Data(contentsOf: dataFilePath!) {
-//            let decoder = PropertyListDecoder()
-//            do {
-//                itemArray = try decoder.decode([Item].self, from: data)
-//            } catch {
-//                print("Error decoding item array, \(error)")
-//            }
-//        }
-//    }
+    func loadItems() {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        do{
+           itemArray = try context.fetch(request)
+        } catch{
+            print("Error fatching data from context, \(error)")
+        }
+    }
 
 }
 
