f98d1df9681a2bf00e96d3f7705d97684c1c6ffb
Dariya Gecher <dariya.gecher@gmail.com>
2023-05-03 23:59:47 +0600

    ff

--- YoTi/AppDelegate.swift
+++ YoTi/AppDelegate.swift
@@ -7,12 +7,29 @@
 
 import UIKit
 import CoreData
+import RealmSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL)
+        
+        let data = Data()
+        data.name = "Dariya"
+        data.age = 18
+        
+        do{
+            let realm = try Realm()
+            try realm.write{
+                realm.add(data)
+            }
+        } catch {
+            print("Error installation new Realm, \(error)")
+        }
+        
         
         return true
     }
---
+++ YoTi/Data Model/Data.swift
@@ -0,0 +1,14 @@
+//
+//  Data.swift
+//  YoTi
+//
+//  Created by Dariya Gecher on 03.05.2023.
+//
+
+import Foundation
+import RealmSwift
+
+class Data: Object {
+    @objc dynamic var name: String = ""
+    @objc dynamic var age: Int = 0
+}
